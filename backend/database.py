757e44673ae6d2049b6b51ff25e578bdfe6a0c2b
29KS <kg062905@gmail.com>
2026-08-20 16:46:20 +0530

    deployment commit 4

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URI, DB_NAME
+from backend.config import MONGO_URI, DB_NAME
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
